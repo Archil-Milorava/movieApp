fc212f50eff67a41fb96444ba4bdd57314751bd9
Archil-Milorava <achimilorava16@gmail.com>
2025-07-01 21:00:53 +0400

    fixed: .env and axios config

--- board/src/config/axiosConfig.ts
+++ board/src/config/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const options = {
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.NODE_ENV === "prod" ? import.meta.env.VITE_API_BASE_URL : import.meta.env.BASE_URL_DEV,
   withCredentials: true,
 };
 
