e042b7f427bea46cb369f9db14e3efe91788208f
Archil-Milorava <achimilorava16@gmail.com>
2025-07-04 01:36:43 +0400

    test commit3

--- board/src/main.tsx
+++ board/src/main.tsx
@@ -7,7 +7,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "./config/queryClient.ts";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+// import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
